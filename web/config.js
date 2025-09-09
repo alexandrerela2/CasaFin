ed92947a7d3dfaf99ba1aca1e8adeca01ae7a39a
alexandrerela2 <alexandrerela2@gmail.com>
2025-09-09 17:16:57 -0300

    Update config.js
    Add Pegue os valores certos no Supabase

--- web/config.js
+++ web/config.js
@@ -1,8 +1,10 @@
 <script>
- 
   window.CASAFIN = {
-    SUPABASE_URL: "https://casafin.vercel.app/",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpaXd1bXFqdW52eHR1ZmVqY2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MzA5MjcsImV4cCI6MjA3MzAwNjkyN30.-70ZnKPoMxUxAMmZ1MoCs-DcTtNmjm0_9x5L3d-03B8",
-       APP_BASE_URL: "http://localhost:8000/web"
+    https://viiwumqjunvxtufejcgf.supabase.co
+    SUPABASE_URL: "https://viiwumqjunvxtufejcgf.supabase.co",
+    eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpaXd1bXFqdW52eHR1ZmVqY2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0MzA5MjcsImV4cCI6MjA3MzAwNjkyN30.-70ZnKPoMxUxAMmZ1MoCs-DcTtNmjm0_9x5L3d-03B8
+    SUPABASE_ANON_KEY: "SUA-ANON-KEY",
+    https://casafin.vercel.app
+    APP_BASE_URL: "https://casafin.vercel.app"
   };
 </script>
